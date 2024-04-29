e15dc52823d2b95ddf25bed2939f3d7a40f4f7f6
Akash chaudhary <146649104+codewithAkash1326@users.noreply.github.com>
2024-04-29 22:10:09 +0530

    Add files via upload

---
+++ Binary tree/112_Path_Sum.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right (nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    bool solve(TreeNode* root, int targetSum,int sum){
+
+        if(root==NULL) return false;
+
+        sum+=root->val;
+
+        if(root->left==NULL && root->right==NULL){
+            if(sum==targetSum){
+                return true;
+            }
+
+            else return false;
+
+
+        }
+
+        bool leftans = solve(root->left,targetSum,sum);
+        bool rightans = solve(root->right,targetSum,sum);
+
+        return leftans|| rightans;
+
+
+
+
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum =0;
+        bool ans = solve(root,targetSum,sum);
+        return ans;
+    }
+};
+int main(){
+ 
+ 
+ 
+return 0;
+}
